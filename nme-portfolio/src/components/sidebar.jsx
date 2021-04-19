5017df4502aa42af6f18bef8cca77d2a88705041
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-04-18 22:31:43 -0400

    a lil more work

--- nme-portfolio/src/components/sidebar.jsx
+++ nme-portfolio/src/components/sidebar.jsx
@@ -8,9 +8,10 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="navbar" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-    
               <h1 id="colorlib-logo"><a href="index.html">Navid Ebrahimi</a></h1>
               <span className="email"><i className="icon-mail"></i> navidmebrahimi22@gmail.com</span>
+              <br/>
+              <span className="phone"><i className="icon-phone"></i> 646-468-5507</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
@@ -18,11 +19,11 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li> <a href="https://www.facebook.com/navid.ebrahimi.10" target="_blank" style={{color: "red"}} rel="noopener noreferrer"><i className="icon-facebook2 bg-warning" />Facebook</a></li>
+                <li> <a href="https://www.facebook.com/navid.ebrahimi.10" target="_blank" style={{color: "red"}} rel="noopener noreferrer"><i className="icon-facebook" />Facebook</a></li>
                 <li> <a href="https://github.com/nme22" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i>Github</a></li>
               </ul>
             </nav>
-            <div className="colorlib-footer">
+            <div className="footer">
             </div>
           </aside>
         </div>
